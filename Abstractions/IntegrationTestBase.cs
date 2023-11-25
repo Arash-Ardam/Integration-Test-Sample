9c3dc40508220433ff2a322b2f6274890c98923b
Arash <120251587+Arash-Ardam@users.noreply.github.com>
2023-11-25 11:22:43 +0330

    Update IntegrationTestBase.cs

--- Abstractions/IntegrationTestBase.cs
+++ Abstractions/IntegrationTestBase.cs
@@ -19,22 +19,10 @@ namespace TPG.SI.TadbirPay.URLTest.Abstractions
     {
         protected readonly TadbirPayWebApplicationFactory _factory;
         protected readonly HttpClient _httpClient;
-        protected HttpClient _IdpHttpClient;
-        public DiscoveryDocumentResponse? _discoveryDocument;
         protected Guid LastTestDoneActionId;
         protected int Tenants;
         private SqlConnection _dbConnection;
-
-        List<string> Scopes = new List<string>()
-            {
-                "openid",
-                "profile",
-                "TadbirPay.Withdrawal.Api.Scope",
-                "TadbirPay.Administrator.Api.Scope",
-                "TadbirPay.Cartable.Api.Scope",
-            };
-        
-
+    
         public string PaymentFile { get; set; }
             = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "Tests", "Payment", "PaymentContent.json"));
 
@@ -52,42 +40,6 @@ namespace TPG.SI.TadbirPay.URLTest.Abstractions
             _factory = factory;
             _httpClient = _factory.CreateClient();
             _httpClient.SetBearerToken("eyJhbGciOiJSUzI1NiIsImtpZCI6IkJEQTFFRURDNjNDQkVDNDY4N0Q5MzdDNThCM0ZBQjYxIiwidHlwIjoiYXQrand0In0.eyJuYmYiOjE2OTg1NzY5ODUsImV4cCI6MTcwMjE3Njk4NSwiaXNzIjoiaHR0cHM6Ly9zaS1sYWItaWRwLmV0YWRiaXIuY29tIiwiYXVkIjpbIlRQRy5TSS5JRFAuQXBpIiwiVFBHLlNJLlRhZGJpclBheS5BcGkiXSwiY2xpZW50X2lkIjoiVGFkYmlyUGF5LkFwaS5Td2FnZ2VyIiwic3ViIjoiNzRjYWNkNjItOGRiNy00N2NiLWI0YjgtYmZkYzY0MWFiMTRiIiwiYXV0aF90aW1lIjoxNjk4MjI4OTYyLCJpZHAiOiJsb2NhbCIsInRlbmFudCI6IlRhZGJpclBhcmRheiIsIm5hbWUiOiLZhdiv24zYsSDYs9uM2LPYqtmFIiwicm9sZSI6WyJjYXJ0YWJsZWFwcHJvdmVyLnJvbGUiLCJXaXRoZHJhd2FsLmNsaWVudC5yb2xlIiwiaXBkcC1hZG1pbiIsImNhcnRhYmxlYWRtaW4ucm9sZSJdLCJlbWFpbCI6ImEucGFuYWhpYW5AZXRhZGJpci5jb20iLCJqdGkiOiJFNEE3OTU5Mjk1MTk0OTE3RUE5NEVGQjRCRTc5Q0U5NCIsInNpZCI6IkM2MjU4MzI1NUIzOUM0RDRDOTIwMTk2OUIyRjVFNzEzIiwiaWF0IjoxNjk4NTc2OTg1LCJzY29wZSI6WyJUYWRiaXJQYXkuQWRtaW5pc3RyYXRvci5BcGkuU2NvcGUiLCJUYWRiaXJQYXkuQ2FydGFibGUuQXBpLlNjb3BlIiwiVGFkYmlyUGF5LldpdGhkcmF3YWwuQXBpLlNjb3BlIiwiVGFkYmlyUGF5LkdhdGV3YXkuQXBpLlNjb3BlIiwiVFBHLlNJLklEUC5BcGkiXSwiYW1yIjpbInB3ZCJdfQ.A4NCfyB4DtCFJMqpE-OTvubn9HY9etNANqKCZ6FBFcf20GwN3QwmKnyPbkry6xargjrN_XJM33FwRPApUrqBTMioQbsnT2Y9Rp7UKfMdglwbQDqXeGUm9V76p_fm10aFm8nzunV5Bf-JsHo2HREn4QMyT1ihazGFP16TnQGWS2Bgq1ktNYiO8BZa35QeHnCQBbzLNN-bNV1mw8SUmqJ3dKLZm8Jip0cOa_HwVRMpiZsLgpz_YLSGK_Zl5oXr1J-k_TF_E-O9m5Y-c5eiD9k7TyCp1IAkCAVWeR7shlvIFF4qwFg15slWtCq6gvD4lK4U3uJZWmE54UHqrnrWFlROgg");
-        }
-
-
-        public async Task SetAccesstokenAsync(HttpClient httpClient)
-        {
-            _IdpHttpClient = _factory.CreateClient();
-
-            //var discoveryDocumentRequest = new DiscoveryDocumentRequest()
-            //{
-            //    Address = "https://si-lab-idp.etadbir.com",
-            //    Policy = new DiscoveryPolicy()
-            //    {
-            //        RequireHttps = true
-            //    }
-            //};
-
-            //_discoveryDocument = await _IdpHttpClient.GetDiscoveryDocumentAsync(discoveryDocumentRequest);
-            PasswordTokenRequest req = new PasswordTokenRequest()
-            {
-                Address = "https://si-lab-idp.etadbir.com/connect/token",
-                ClientId = "TadbirPay.Api.Swagger",
-                UserName = "Ali",
-                Password = "Aa#123456",
-                Scope = string.Join(" ", Scopes)
-            };
-
-            var tokenResponse = await _IdpHttpClient.RequestPasswordTokenAsync(req);
-
-            if (tokenResponse.IsError)
-            {
-                throw new Exception("Un Auth");
-            }
-
-            httpClient.SetBearerToken(tokenResponse.AccessToken);
-
-
         }
 
         protected Uri CreatePaymentBaseRoute(string route)
@@ -116,62 +68,7 @@ namespace TPG.SI.TadbirPay.URLTest.Abstractions
         }
 
         #region Get Id By sqlConnection methods
-        protected async Task GetPaymentIdFromConnectionString() 
-        {
-            string result = string.Empty;
-
-            var connectionString = "Server=.;Database=TPG.SI.TadbirPay;User Id=sa;Password=a`123456;TrustServerCertificate=True";
-
-            using (var DbConnection = new SqlConnection(connectionString))
-            {
-                using (var command = DbConnection.CreateCommand())
-                {
-                    command.CommandText = "use [TadbirPay.TadbirPardaz] select Id from dbo.WithdrawalOrder where Name = 'test' ";
-
-                    await DbConnection.OpenAsync();
-                    using (var reader = await command.ExecuteReaderAsync())
-                    {
-                        if (await reader.ReadAsync())
-                        {
-                            result = reader.GetString(0);
-                        }
-
-                        if (await reader.ReadAsync())
-                        {
-                            throw new Exception();
-                        }
-                    }
-                }
-
-                await DbConnection.CloseAsync();
-            }
-
-            LastTestDoneActionId = Guid.Parse(result);
-        }
-
-        public async Task<int> getTenantsCount()
-        {
-            var query = "use [TPG.SI.TadbirPay] select COUNT(*) from dbo.Tenant";
-
-            InitDbConnection();
-            await EnsureDbConnectionOpen();
-            await ExecuteQueryAsync(query);
-            await CloseConnectionAsync();
-
-            return this.Tenants;
-        }
-
-        public async Task ExecuteQueryAsync(string query)
-        {
-            using (var command = _dbConnection.CreateCommand())
-            {
-                command.CommandText = query;
-                //this.Tenants = (await command.ExecuteReaderAsync()).GetString(0);
-                var result = await command.ExecuteScalarAsync();
-                this.Tenants = (int)result;
-
-            }
-        }
+        
         /// <summary>
         /// Get data from db by executing specified query
         /// </summary>
@@ -254,7 +151,7 @@ namespace TPG.SI.TadbirPay.URLTest.Abstractions
 
         public void InitDbConnection()
         {
-            var connectionString = "Server=172.16.30.144,1433;Database=TPG.SI.TadbirPay;User Id=si-user;Password=tadbir@123;TrustServerCertificate=True";
+            var connectionString = "Server=.;Database=TPG.SI.TadbirPay;User Id=si-user;Password=tadbir@123;TrustServerCertificate=True";
 
             _dbConnection = new SqlConnection(connectionString);            
         }
